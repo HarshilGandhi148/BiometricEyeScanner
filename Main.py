d25e5fd30848995b9ab3b9e8180a54153684b18c
HarshilGandhi148 <harshilgandhi148@gmail.com>
2023-06-10 13:38:45 -0400

    Added base tesseract code

--- Main.py
+++ Main.py
@@ -1,3 +1,10 @@
 import pytesseract as tess
 from PIL import Image
 import cv2
+
+tess.pytesseract.tesseract_cmd = r'<insert path to tesseract executable here>'
+
+image = Image.open(r'<insert path to image here>')
+imageText = tess.image_to_string(image)
+
+print(imageText)
