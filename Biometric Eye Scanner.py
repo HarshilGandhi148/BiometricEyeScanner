aba9cc96be798bd344dbe13743ed105a8af79d58
HarshilGandhi148 <harshilgandhi148@gmail.com>
2023-10-10 17:44:45 -0400

    True/False eye detection + text on screen

--- Biometric Eye Scanner.py
+++ Biometric Eye Scanner.py
@@ -4,16 +4,14 @@ face_classifier = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_eye.xml"
 )
 
+eye = False
 
 video = cv2.VideoCapture(0)
 
-def create_face_box(image):
+def detect_eyes(image):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize = (40, 40))
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 4)
-    return faces
-
+    return not(faces == ())
 
 while True:
     result, frame = video.read()
@@ -21,13 +19,9 @@ while True:
     if result is False:
         break
 
-    faces = create_face_box(
-        frame
-    )
-
-    cv2.imshow(
-        "Face Detection", frame
-    )
+    eye = detect_eyes(frame)
+    frame = cv2.putText(frame, str(eye), (50, 50) , cv2.FONT_HERSHEY_SIMPLEX , 1,(0, 255, 0), 2, cv2.LINE_AA) 
+    cv2.imshow("Eye Detection", frame)
 
     k = cv2.waitKey(1) & 0xFF
     #esc
