4790d21e88f09b14d5bfa3b73a0bedef0ca60cbd
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2023-10-28 19:42:35 -0400

    Update eyetracker.py

--- eyetracker.py
+++ eyetracker.py
@@ -26,11 +26,6 @@ def contour(thresh, mid, img, right = False):
         cy = int(M['m01']/M['m00'])
         if right:
             cx += mid
-            print(cx - rightMidX)
-            print(cy - rightMidY)
-        else:
-            print(cx - leftMidX)
-            print(cy - leftMidY)
         cv2.circle(img, (cx, cy), 4, (255, 255, 255), 2)
     except:
         idle(0)
@@ -41,29 +36,12 @@ predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 left = [36, 37, 38, 39, 40, 41]
 right = [42, 43, 44, 45, 46, 47]
 
-leftXSum = 0
-leftYSum = 0
-rightXSum = 0
-rightYSum = 0
-leftMidX = 0
-leftMidY = 0
-rightMidX = 0
-rightMidY = 0
-
-cap = cv2.VideoCapture(0)
-ret, img = cap.read()
-thresh = img.copy()
-
 kernel = np.ones((9, 9), np.uint8)
 
 def detectEyes(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rects = detector(gray, 1)
     for rect in rects:
-        leftXSum = 0
-        leftYSum = 0
-        rightXSum = 0
-        rightYSum = 0
         shape = predictor(gray, rect)
         shape = shapeToNPArray(shape)
         mask = np.zeros(img.shape[:2], dtype=np.uint8)
@@ -83,29 +61,9 @@ def detectEyes(img):
         thresh = cv2.bitwise_not(thresh)
         for (x, y) in shape[36:42]:
             cv2.circle(img, (x, y), 2, (0, 255, 0), -1)
-            leftXSum += x
-            leftYSum += y
         for (x, y) in shape[42:48]:
             cv2.circle(img, (x, y), 2, (0, 255, 0), -1)
-            rightXSum += x
-            rightYSum += y
-        leftMidX = leftXSum / 6
-        leftMidY = leftYSum / 6
-        rightMidX = rightXSum / 6
-        rightMidY = rightYSum / 6
         contour(thresh[:, 0:mid], mid, img)
         contour(thresh[:, mid:], mid, img, True)
 
     return(img)
-
-while(True):
-    ret, img = cap.read()
-    img = detectEyes(img)
-        
-    cv2.imshow("eyes", img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-cap.release()
-cv2.destroyAllWindows()
-
